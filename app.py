e685cc2f86725656c9da0f508d56b999557272e6
GabrielCoGa <gabriel.cortes@hotmail.com>
2025-11-15 18:22:03 +0100

    menu de inicio

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 #https://youtu.be/4yEKWer4cVI?si=hZPuBqtvZYlgMv8p
+import Listdatabase
+USER_CHOICE = """
+Enter:
+- 'a' to add a new book
+_ 'l' to list all books
+_ 'r' to mark a book as read
+_ 'd' to delete a book
+- 'q' to quit
+
+Your choice:"""
+
+def menu():
+    user_input = input(USER_CHOICEÇ)
+    while user_input != 'q':
+        pass
